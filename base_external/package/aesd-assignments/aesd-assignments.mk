018962fa6008cbc0e92222a4a5c3b97fc51bb1d6
temp <temp@Ubuntu.myguest.virtualbox.org>
2025-03-31 16:00:52 -0600

    Updated Git hash of A3 repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 03eba2478abdd5fb850862e45d2935bb132659cb
+AESD_ASSIGNMENTS_VERSION = c06aada057563c83598612264d3d6cadb5610e29
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
